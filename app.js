60a5146d29d95008e3e8d33e8c68f878b6fb4091
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-23 10:53:32 +0700

    make a map using d3 topojson and svg

--- app.js
+++ app.js
@@ -11,3 +11,34 @@ d3.queue()
         year: +row.Year
     }
 })
+.await(function(error, mapData, data){
+    if (error) throw error;
+
+    var extremeYears = d3.extent(data, d => d.year);
+    var currentYear = extremeYears[0];
+    var currentDataType = d3.select('input[name="data-type"]:checked')
+                                .attr("value");
+    var geoData = topojson.feature(mapData, mapData.objects.countries).features;
+    var width = +d3.select(".chart-container")
+                            .node().offsetWidth;
+    var height = 300;
+    
+    createMap(width, width * 4 / 5);
+    drawMap(geoData, data, currentYear, currentDataType);
+
+    d3.select('#year')
+        .property("min", currentYear)
+        .property("max", extremeYears[1])
+        .property("value", currentYear)
+        .on("input", () => {
+            currentYear = +d3.event.target.value;
+            drawMap(geoData, data, currentYear, currentDataType);
+        });
+
+    d3.selectAll('input[name="data-type"]')
+        .on("change", () => {
+            currentDataType = d3.event.target.value;
+            drawMap(geoData, data, currentYear, currentDataType);
+        });
+
+})
