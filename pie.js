2242b8338b7bb0be0d23e255eaf0be36a0a5cc3f
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-23 13:10:37 +0700

    use general update pattern to create and update piechart

--- pie.js
+++ pie.js
@@ -16,5 +16,52 @@ function createPie(width, height) {
 }
 
 function drawPie(data, currentYear) {
+    var pie = d3.select("#pie");
+
+    var arcs = d3.pie()
+                .sort((a, b) => {
+                    if(a.continent < b.continent) return -1;
+                    if(a.continent > b.continent) return 1;
+                    return a.emissions - b.emissions;
+                })
+                .value(d => d.emissions);
+
+    var path = d3.arc()
+                    .outerRadius(+pie.attr("height") / 2 - 50)
+                    .innerRadius(10);
+
+    var yearData = data.filter(d => d.year === currentYear);
+    var continents = [];
+    for (var i = 0; i < yearData.length; i++){
+        var continent = yearData[i].continent;
+        if (!continents.includes(continent)) {
+            continents.push(continent);
+        }
+    }
+    var colorScale = d3.scaleOrdinal()
+                        .domain(continents)
+                        .range(["#ab47bc", "#7e57c2", "#26a69a", "#42a5f5", "#78909c"]);
+    var update = pie
+                    .select(".chart")
+                    .selectAll(".arc")
+                    .data(arcs(yearData));
+                    
+
+    update 
+        .exit()
+        .remove();
     
+    update  
+        .enter()
+            .append("path")
+            .classed("arc", true)
+            .attr("stroke", "#dff1ff")
+            .attr("stroke-width", "0.25px")
+        .merge(update)
+            .attr("fill", d => colorScale(d.data.continent))
+            .attr("d", path);
+
+
+    pie.select(".pie-title")
+            .text("Total emissions by continent and region")
 }
